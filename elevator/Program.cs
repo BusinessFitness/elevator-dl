912a86f32c7f8347d8805c76730c64c39221180e
timothyoverton <timothy.overton@gmail.com>
2016-12-14 12:41:55 +1000

    First commit with test project added

---
+++ elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elevator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello I'm {GetElevatorName()}. How can I help you");
+            Console.ReadLine();
+        }
+
+        public static string GetElevatorName()
+        {
+            return "Elevator 01";
+        }
+    }
+}
